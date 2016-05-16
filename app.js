568ef957247ce978e732974e66c31f402c3230e2
Ebony <OIC8@OICs-MacBook-Air-4.local>
2016-05-16 15:45:07 -0400

    ejcd

---
+++ app.js
@@ -0,0 +1,65 @@
+$('.content').hide();
+
+$(".link").click(function(){$('.content').show();});
+
+$(".close-classic").click(function(){
+  $(".content").slideUp("slow");
+  $('.close-box').hide("slow");
+});
+
+
+$(".link").click(function() {
+$(".content-container").css({"top":"20%"});
+});
+
+
+$("#link-1").click(function(){
+  $("#content-1").slideToggle("slow");
+  $('.close-box').show("slow");
+  $("#content-2").slideUp("slow");
+  $("#content-3").slideUp("slow");
+  $("#content-4").slideUp("slow");
+  $("#content-5").slideUp("slow");
+  });
+
+$("#link-2").click(function(){
+  $("#content-2").slideToggle("slow");
+  $('.close-box').show("slow");
+  $("#content-1").slideUp("slow");
+  $("#content-3").slideUp("slow");
+  $("#content-4").slideUp("slow");
+  $("#content-5").slideUp("slow");
+  });
+
+$("#link-3").click(function(){
+  $("#content-3").slideToggle("slow");
+  $('.close-box').show("slow");
+  $("#content-2").slideUp("slow");
+  $("#content-1").slideUp("slow");
+  $("#content-4").slideUp("slow");
+  $("#content-5").slideUp("slow");
+  });
+
+$("#link-4").click(function(){
+  $("#content-4").slideToggle("slow");
+  $('.close-box').show("slow");
+  $("#content-2").slideUp("slow");
+  $("#content-3").slideUp("slow");
+  $("#content-1").slideUp("slow");
+  $("#content-5").slideUp("slow");
+  });
+
+$("#link-5").click(function(){
+  $("#content-5").slideToggle("slow");
+  $('.close-box').show("slow");
+  $("#content-2").slideUp("slow");
+  $("#content-3").slideUp("slow");
+  $("#content-4").slideUp("slow");
+  $("#content-1").slideUp("slow");
+  });
+
+
+$(".scroller").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
